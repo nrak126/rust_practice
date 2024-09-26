b489365a9e9eb6b51eca706723553b3eedf84938
nrak126 <daichidaichi126@gmail.com>
2024-09-26 00:44:33 +0000

    フィボナッチのアルゴリズムを改善

--- fibonacci/src/main.rs
+++ fibonacci/src/main.rs
@@ -1,14 +1,22 @@
+use std::collections::HashMap;
+
 fn main() {
-    let x: u32 = 6;
-    println!("fibonatch({}) = {}", x, fibonatch(x));
+    let mut cache: HashMap<u32, u32> = HashMap::new();
+
+    cache.insert(0, 0);
+    cache.insert(1, 1);
+
+    let x: u32 = 40;
+    println!("fibonacci({}) = {}", x, fibonacci(x, &mut cache));
 }
 
-fn fibonatch(n: u32) -> u32 {
-    if n == 0 {
-        0
-    } else if n == 1 {
-        1
-    } else {
-        fibonatch(n - 1) + fibonatch(n - 2)
+fn fibonacci(n: u32, cache: &mut HashMap<u32, u32>) -> u32 {
+    if let Some(&value) = cache.get(&n) {
+        return value;
     }
+
+    let result = fibonacci(n - 1, cache) + fibonacci(n - 2, cache);
+    cache.insert(n, result);
+
+    result
 }
